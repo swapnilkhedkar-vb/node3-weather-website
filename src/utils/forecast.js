3f58db4e2b98bff76fd77588b2a7cdc5b296e107
Swapnilkhedkar <swapnilkhedkar@valuebound.com>
2022-05-03 12:43:07 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require("postman-request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=f03ec50459be2492efab928fcd9cb29f&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=f";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to server", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          `. It is currently ${body.current.temperature} degrees out there. It feels like ${body.current.feelslike} degrees`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
