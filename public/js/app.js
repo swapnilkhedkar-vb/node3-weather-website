b15e38e3be2d0ee881bbc7ead57e2b70df180288
Swapnilkhedkar <swapnilkhedkar@valuebound.com>
2022-05-03 12:44:35 +0530

    Removed console log from public/js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client Side JS File Loaded");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.getElementById("message-1");
