7d5c45fa1801848aee277f2df81a174e531665ed
Swapnilkhedkar <swapnilkhedkar@valuebound.com>
2022-05-03 13:35:38 +0530

    added humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,7 +17,7 @@ const forecast = (latitude, longitude, callback) => {
       callback(
         undefined,
         body.current.weather_descriptions[0] +
-          `. It is currently ${body.current.temperature} degrees out there. It feels like ${body.current.feelslike} degrees`
+          `. It is currently ${body.current.temperature} degrees out there. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`
       );
     }
   });
